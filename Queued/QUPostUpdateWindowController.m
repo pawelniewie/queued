a2d2d510496cc9fc44672c041ffaa73720c2fb8e
Pawel Niewiadomski <11110000b@gmail.com>
2013-07-14 22:42:46 +0200

    added QUPostUpdateWindowController enabled Objective-C modules

---
+++ Queued/QUPostUpdateWindowController.m
@@ -0,0 +1,34 @@
+//
+//  QUPostUpdateWindowController.m
+//  Queued
+//
+//  Created by Pawel Niewiadomski on 14.07.2013.
+//  Copyright (c) 2013 Pawel Niewiadomski. All rights reserved.
+//
+
+#import "QUPostUpdateWindowController.h"
+
+@interface QUPostUpdateWindowController ()
+
+@end
+
+@implementation QUPostUpdateWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
